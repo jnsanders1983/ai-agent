831d4f00ec89c0f922c10169212127694791795c
jnsanders1983 <jnsanders1983@gmail.com>
2026-07-10 14:47:38 -0500

    Token Metadata

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ def main():
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
-    )  
+    )
 
     response = client.chat.completions.create(
         model= "openrouter/free",
-        messages=[
+        messages= [
             {
                 "role": "user",
                 "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
             }
-        ])
+        ],
+    )
     
+    if not response.usage:
+        raise RuntimeError("API response appears to be malformed")
+    
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
     print("Response:")
     print(response.choices[0].message.content)
 
